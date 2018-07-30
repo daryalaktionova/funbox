6693ff9641b558d9d2a53d4f7ee81c473292089c
daryalaktionova <segezmundovna@gmail.com>
2018-07-31 00:19:07 +0300

    Add js disabled

--- js/index.js
+++ js/index.js
@@ -1,7 +1,23 @@
-console.log('test');
 const items = Array.prototype.slice.call(document.querySelectorAll('.yummy__item'));
-console.log(items);
-
 items.forEach((item) => {
-    const checkbox = product.querySelector('input[type=checkbox]');
-});
+    const checkbox = item.querySelector('input[type=checkbox]');
+    const buy = item.querySelector('.yummy__item-description');
+    const card = item.querySelector('.yummy__label');
+    
+    const handlerSatusProduct = () => {
+        if (!checkbox.hasAttribute('checked')) {
+          checkbox.setAttribute('checked', true);
+          item.classList.add('yummy_item--selected');
+        } else {
+          checkbox.removeAttribute('checked');
+          item.classList.remove('yummy_item--selected');
+        }
+      };
+
+      if (checkbox.hasAttribute('disabled')) {
+        card.classList.add('yummy__label--disabled');
+        item.classList.add('yummy_item--disabled');
+        buy.innerHTML = item.dataset.disabled;
+      }
+    
+    });
