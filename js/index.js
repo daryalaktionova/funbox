4f535a2b9d6b552bdc6c24f435dd0d69999f5306
daryalaktionova <segezmundovna@gmail.com>
2018-07-30 23:25:16 +0300

    Add tablerts wrapper

--- js/index.js
+++ js/index.js
@@ -3,5 +3,5 @@ const items = Array.prototype.slice.call(document.querySelectorAll('.yummy__item
 console.log(items);
 
 items.forEach((item) => {
-
+    const checkbox = product.querySelector('input[type=checkbox]');
 });
