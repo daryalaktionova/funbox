a61ffccf7af25a7698d9a36286f63a9c60ba934e
daryalaktionova <segezmundovna@gmail.com>
2018-07-31 22:50:25 +0300

    Add hover

--- js/index.js
+++ js/index.js
@@ -1,25 +1,41 @@
 const items = Array.prototype.slice.call(document.querySelectorAll('.yummy__item'));
 items.forEach((item) => {
     const checkbox = item.querySelector('input[type=checkbox]');
-    const buy = item.querySelector('.yummy__item-description');
+    const desc = item.querySelector('.yummy__item-description');
     const card = item.querySelector('.yummy__label');
-    
+    const caption = item.querySelector('.yummy__item-caption');
+
     const selectItems = () => {
-        if (!checkbox.hasAttribute('checked')) {
+        if ((!checkbox.hasAttribute('checked')) && (!checkbox.hasAttribute('disabled'))) {
           checkbox.setAttribute('checked', true);
           item.classList.add('yummy_item--selected');
+          item.classList.add('selected');
           card.classList.add('yummy__label--selected');
+          caption.removeAttribute('hidden');
+          desc.setAttribute('hidden', true);
         } else {
           checkbox.removeAttribute('checked');
           item.classList.remove('yummy_item--selected');
+          item.classList.remove('selected');
+          item.classList.remove('selected-hover');
           card.classList.remove('yummy__label--selected');
+          caption.setAttribute('hidden', true);
+          desc.removeAttribute('hidden');
         }
       };
 
       if (checkbox.hasAttribute('disabled')) {
         card.classList.add('yummy__label--disabled');
         item.classList.add('yummy_item--disabled');
-        buy.innerHTML = item.dataset.disabled;
+        desc.innerHTML = item.dataset.disabled;
       }
       checkbox.addEventListener('change', selectItems);
+      card.addEventListener('click', selectItems);
+      card.addEventListener('mouseleave', () => {
+        if (checkbox.hasAttribute('checked')) {
+          item.classList.add('selected-hover');
+        } else {
+          item.classList.remove('selected-hover');
+        }
+      });
     });
